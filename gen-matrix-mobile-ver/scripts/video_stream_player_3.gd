859599d2e431593944640c6c44ff2d684b6f8040
An Pham <anpham.0992@gmail.com>
2026-06-11 10:32:36 -0400

    ok only theme 1 doesnt work so dont need to go back

--- gen-matrix-mobile-ver/scripts/video_stream_player_3.gd
+++ gen-matrix-mobile-ver/scripts/video_stream_player_3.gd
@@ -4,7 +4,8 @@ extends VideoStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player.spawn_joystick()
+	#player.spawn_joystick()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
